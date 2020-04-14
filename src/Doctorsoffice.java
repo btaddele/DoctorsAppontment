da376396f7210e0fb18b438a520caa6c413a557e
btaddele <Betelhem.biresaw@gmail.com>
2020-04-14 17:11:08 -0400

    first commit

---
+++ src/Doctorsoffice.java
@@ -0,0 +1,26 @@
+public class Doctorsoffice {
+    public static void main(String[] args) {
+
+        String Name = "Stacey Manson";
+        boolean PatientHasInsurance = false;
+        double BodyTemprature = 102.2;
+        int NumberOfVisits = 5;
+        int copayAmount = 130;
+        String ContactNumber ="123-456-7890";
+        String next_appointment_date = "10-11-2020";
+
+
+
+
+        System.out.println("Patient Details:");
+        System.out.println("name:" + Name);
+        System.out.println("Patient has insurance:" + PatientHasInsurance);
+        System.out.println("Body temperature: " + BodyTemprature);
+        System.out.println("Number of visits: " + NumberOfVisits);
+        System.out.println("copay Amount: " + copayAmount);
+        System.out.println("ContactNumber: " + ContactNumber);
+        System.out.println("next appointment date: " + next_appointment_date);
+
+
+    }
+}
